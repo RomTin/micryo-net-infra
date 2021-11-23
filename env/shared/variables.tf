f7531379beed260177da06d4c69482c694ce05ac
RimTin <micfall.romtin@gmail.com>
2021-11-23 19:35:24 +0900

    fix: AthenaID

--- env/shared/variables.tf
+++ env/shared/variables.tf
@@ -2,6 +2,6 @@
 
 locals {
   cost_limit         = 20 # 請求額の上限値（USD）
-  athena_id          = "logs-micryo-net"
+  athena_id          = "logs_micryo_net"
   athena_bucket_name = "athena-data-bucket-micryo-net"
 }
