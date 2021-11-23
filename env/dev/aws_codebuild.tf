04b700dab46455623ab250411248f1f90eda6448
RimTin <micfall.romtin@gmail.com>
2021-11-24 00:55:03 +0900

    feat: devにCodeBuildを追加した

--- env/dev/aws_codebuild.tf
+++ env/dev/aws_codebuild.tf
@@ -1,5 +1,16 @@
 # aws_codebuild.tf
 
-//module "cd" {
-//  source = "../../module/codebuild_cd"
-//}
+module "cd" {
+  source                = "../../module/codebuild_cd"
+  env                   = local.env
+  compute_type          = "BUILD_GENERAL1_SMALL"
+  repository_name       = "micryo-net-blog"
+  repository_url        = "https://github.com/RomTin/micryo-net-blog"
+  buildspec_path        = "buildspec.dev.yml"
+  role_arn              = data.aws_iam_role.cd_role.arn
+  environment_variables = []
+  webhook_filter_group = [
+    { type = "EVENT", pattern = "PUSH", exclude_matched_pattern = false },
+    { type = "HEAD_REF", pattern = "refs/heads/develop", exclude_matched_pattern = false }
+  ]
+}
